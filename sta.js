15fdccb4520d879302a092375c6b09a532ba2180
jee-mj <miraj@bhattarai.com.au>
2023-12-02 14:56:53 +1100

    [done] timer queue [next] io queue

--- sta.js
+++ sta.js
@@ -1,3 +1,17 @@
+setTimeout(() => console.log("this is setTimeout() 1"), 1000);
+setTimeout(() => console.log("this is setTimeout() 2"), 500);
+setTimeout(() => console.log("this is setTimeout() 3"), 0);
+
+// // setTimeout() and nextTick() interactions
+// setTimeout(() => console.log("this is setTimeout() 1"), 0);
+// setTimeout(() => {
+//   console.log("this is setTimeout() 2");
+//   process.nextTick(() => {
+//     console.log("this is the inner nextTick() inside setTimeout()");
+//   });
+// }, 0);
+// setTimeout(() => console.log("this is setTimeout() 3"), 0);
+
 // /*
 //   Nested microtask queue
 // */
@@ -7,7 +21,7 @@
 // process.nextTick(() => {
 //   console.log("this is process.nextTick2()");
 //   process.nextTick(() =>
-//     console.log("this is the inner next tick inside process.nextTick()")
+//     console.log("this is the inner nextTick() inside process.nextTick()")
 //   );
 // });
 // process.nextTick(() => console.log("this is process.nextTick3()"));
@@ -16,7 +30,7 @@
 // Promise.resolve().then(() => {
 //   console.log("This is promise.resolve.then() 2");
 //   process.nextTick(() =>
-//     console.log("this is the inner next tick inside promise.resolve.then()")
+//     console.log("this is the inner nextTick() inside promise.resolve.then()")
 //   );
 // });
 // Promise.resolve().then(() => console.log("this is promise.resolve.then() 3"));
@@ -25,7 +39,7 @@
 // Promise.resolve().then(() => console.log("this is promise.resolve 1"));
 // process.nextTick(() => console.log("this is process.nextTick1"));
 
-// async demonstration
-console.log("this is console.log 1");
-process.nextTick(() => console.log("this is process.nextTick1"));
-console.log("this is console.log 3");
+// // async demonstration
+// console.log("this is console.log 1");
+// process.nextTick(() => console.log("this is process.nextTick1"));
+// console.log("this is console.log 3");
