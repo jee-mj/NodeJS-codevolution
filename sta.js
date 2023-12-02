b900ed267980fc5ea84770ca10648bfe6e776e07
jee-mj <miraj@bhattarai.com.au>
2023-12-02 15:11:34 +1100

    [done] io queue [next] io polling

--- sta.js
+++ sta.js
@@ -1,45 +1,29 @@
-setTimeout(() => console.log("this is setTimeout() 1"), 1000);
-setTimeout(() => console.log("this is setTimeout() 2"), 500);
-setTimeout(() => console.log("this is setTimeout() 3"), 0);
+const fs = require("node:fs");
 
-// // setTimeout() and nextTick() interactions
-// setTimeout(() => console.log("this is setTimeout() 1"), 0);
-// setTimeout(() => {
-//   console.log("this is setTimeout() 2");
-//   process.nextTick(() => {
-//     console.log("this is the inner nextTick() inside setTimeout()");
-//   });
-// }, 0);
-// setTimeout(() => console.log("this is setTimeout() 3"), 0);
+fs.readFile(__filename, () => {
+  console.log("this is fs.readFile() 1"); // fourth
+});
+process.nextTick(() => console.log("this is process.nextTick() 1")); // first
+Promise.resolve().then(() => console.log("this is Promise.resolve.then() 1")); // third
+setTimeout(() => console.log("this is setTimeout() 1"), 0); // second
 
-// /*
-//   Nested microtask queue
-// */
+for (let i = 0; i < 2000000000; i++) {}
 
-// // nextTick
-// process.nextTick(() => console.log("this is process.nextTick1()"));
-// process.nextTick(() => {
-//   console.log("this is process.nextTick2()");
-//   process.nextTick(() =>
-//     console.log("this is the inner nextTick() inside process.nextTick()")
-//   );
+// // outer loop can be executed in any order
+// const fs = require("node:fs");
+
+// setTimeout(() => console.log("this is setTimeout() 1"), 0); // 0 overrides to 1 ms
+
+// fs.readFile(__filename, () => {
+//   console.log("this is fs.readFile() 1");
 // });
-// process.nextTick(() => console.log("this is process.nextTick3()"));
-// // Promise
-// Promise.resolve().then(() => console.log("this is promise.resolve.then() 1"));
-// Promise.resolve().then(() => {
-//   console.log("This is promise.resolve.then() 2");
-//   process.nextTick(() =>
-//     console.log("this is the inner nextTick() inside promise.resolve.then()")
-//   );
+
+// // microtask queue has higher priority than io queue
+// const fs = require("node:fs");
+
+// fs.readFile(__filename, () => {
+//   console.log("this is fs.readFile() 1");
 // });
-// Promise.resolve().then(() => console.log("this is promise.resolve.then() 3"));
 
-// // promise waits for code to resolve then excutes demonstration
-// Promise.resolve().then(() => console.log("this is promise.resolve 1"));
-// process.nextTick(() => console.log("this is process.nextTick1"));
-
-// // async demonstration
-// console.log("this is console.log 1");
-// process.nextTick(() => console.log("this is process.nextTick1"));
-// console.log("this is console.log 3");
+// process.nextTick(() => console.log("this is process.nextTick() 1"));
+// Promise.resolve().then(() => console.log("this is Promise.resolve.then() 1"));
